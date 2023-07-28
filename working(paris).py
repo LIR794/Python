ec20aa7092465c42b0c8d4d5079ca259252eed17
LIR794 <krisoediz@mail.ru>
2023-07-28 15:31:59 +0300

    Finally

--- working(paris).py
+++ working(paris).py
@@ -1,65 +1,77 @@
 import json
+import requests
 from openpyxl import load_workbook
 
 # Загрузка таблицы
-workbook = load_workbook('Вставить путь до таблицы')
-sheet = workbook.active
+workbook = load_workbook('Укажите путь до файла')
 
-previous_value = None  # Переменная для хранения предыдущего значения
 results = []  # Список для хранения результатов
 
-# Поиск значений 0 и вывод информации о ячейках
-for column in sheet.iter_cols():
-    for cell in column:
-        if cell.value == 0:
-            row_above = cell.row - 2
-            day_of_week = sheet.cell(row=cell.row, column=cell.column - 1).value
-            day_mapping = {
-                'понедельник': 'Monday',
-                'вторник': 'Tuesday',
-                'среда': 'Wednesday',
-                'четверг': 'Thursday',
-                'пятница': 'Friday',
-                'суббота': 'Saturday',
-                'воскресенье': 'Sunday'
-            }
-            day_of_week_eng = day_mapping.get(day_of_week)
-            value_above = sheet.cell(row=row_above, column=cell.column).value
-            previous_value = sheet.cell(row=row_above, column=cell.column).value if value_above != 5 else previous_value
+# Проход по всем листам в книге
+for sheet_name in workbook.sheetnames:
+    sheet = workbook[sheet_name]
 
-            if value_above == 5:
-                output = {"groupName": previous_value, "weekDay": day_of_week_eng, "paris": []}
-            else:
-                output = {"groupName": value_above, "weekDay": day_of_week_eng, "paris": []}
+   
 
-            # Добавление значений из соседних ячеек
-            for i in range(0, 6):  # 6 строк вниз
-                pair_column = cell.column + 1
-                room_column = cell.column + 2
-                pair_number_column = cell.column
+    previous_value = None  # Переменная для хранения предыдущего значения
+    for column in sheet.iter_cols():
+        for cell in column:
+            if cell.value == 0:
+                row_above = cell.row - 2
+                day_of_week = sheet.cell(row=cell.row, column=cell.column - 1).value
+                day_mapping = {
+                    'понедельник': 'monday',
+                    'вторник': 'tuesday',
+                    'среда': 'wednesday',
+                    'четверг': 'thursday',
+                    'пятница': 'friday',
+                    'суббота': 'saturday',
+                    'воскресенье': 'sunday'
+                }
+                day_of_week_eng = day_mapping.get(day_of_week)
+                value_above = sheet.cell(row=row_above, column=cell.column).value
+                previous_value = sheet.cell(row=row_above, column=cell.column).value if value_above != 5 else previous_value
 
-                pair_value = sheet.cell(row=cell.row + i, column=pair_column).value
-                room_value = sheet.cell(row=cell.row + i, column=room_column).value
-                pair_number_value = sheet.cell(row=cell.row + i, column=pair_number_column).value
+                if value_above == 5:
+                    output = {"groupName": previous_value, "weekDay": day_of_week_eng, "pairs": []}
+                else:
+                    output = {"groupName": value_above, "weekDay": day_of_week_eng, "pairs": []}
 
-                if pair_value is not None:
-                    pair_value = ' '.join(pair_value.split())
-                    if room_value and room_value != 0:
-                        room_value = ' '.join(str(room_value).split())
-                        row_values = {"pairNum": pair_number_value, "pairName": pair_value, "pairCab": room_value}
-                    else:
-                        row_values = {"pairNum": pair_number_value, "pairName": pair_value}
-                    output["paris"].append(row_values)
+                # Добавление значений из соседних ячеек
+                for i in range(0, 6):  # 6 строк вниз
+                    pair_column = cell.column + 1
+                    room_column = cell.column + 2
+                    pair_number_column = cell.column
 
-            if output["paris"]:
-                results.append(output)
+                    pair_value = sheet.cell(row=cell.row + i, column=pair_column).value
+                    room_value = sheet.cell(row=cell.row + i, column=room_column).value
+                    pair_number_value = sheet.cell(row=cell.row + i, column=pair_number_column).value
 
+                    if pair_value is not None:
+                        pair_value = ' '.join(pair_value.split())
+                        if room_value and room_value != 0:
+                            room_value = ' '.join(str(room_value).split())
+                            row_values = {"pairNum": pair_number_value, "pairName": pair_value, "pairCab": room_value}
+                        else:
+                            row_values = {"pairNum": pair_number_value, "pairName": pair_value, "pairCab": ""}
+                        output["pairs"].append(row_values)
+
+                if output["pairs"]:
+                    results.append(output)
+
+
+payload = {
+    "password": "укажите пароль с send запроса",
+    "groups": results
+}
+
+
+json_payload = json.dumps(payload, separators=(",", ":"), ensure_ascii=False)
+
+    # Отправка файла по ссылке
+server_url = "ссылка на send запрос"  # Замените на вашу ссылку
+response = requests.post(server_url, json=payload, headers={'Content-Type': 'application/json'})
+print(f"Отправка результатов на сайт для листа '{sheet_name}': {response.status_code} - {response.text}")
+ 
 # Закрытие таблицы
 workbook.close()
-
-# Изменение пути сохранения файла JSON
-output_path = "путь"
-
-# Сохранение в файл JSON с отступами
-with open(output_path, "w", encoding="utf-8") as file:
-    json.dump(results, file, indent=4, ensure_ascii=False)
